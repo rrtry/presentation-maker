5a41075138803231bd160bfbb1d8238ff283e4a7
= <fedormocalov36@gmail.com>
2025-01-12 16:37:08 +0300

    dev-24

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,20 @@ import { TopPanel } from "./view/topPanel/TopPanel.tsx";
 import { Workspace } from "./view/Workspace.tsx";
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { HistoryType } from './utils/history.ts';
+import { HistoryContext } from './view/slide/historyContext.ts';
 
 function App() {
     return (
-        <DndProvider backend={HTML5Backend}>
-            <TopPanel></TopPanel>
-            <main className={styles.container}>
-                <SlidesList></SlidesList>
-                <Workspace></Workspace>
-            </main>
-        </DndProvider>
+        <HistoryContext.Provider value={history}>
+            <DndProvider backend={HTML5Backend}>
+                <TopPanel></TopPanel>
+                <main className={styles.container}>
+                    <SlidesList></SlidesList>
+                    <Workspace></Workspace>
+                </main>
+            </DndProvider>
+        </HistoryContext.Provider>
     );
 }
 
